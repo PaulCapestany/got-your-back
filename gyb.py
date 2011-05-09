43920ad8f3694edfc809352b89bc5166b75751f1
jlee@pbu.edu <jlee@pbu.edu@0639aa0d-7a2e-ecb4-53b4-565c565d6c1b>
2011-05-09 16:04:00 +0000

    -Alpha 7
    git-svn-id: http://got-your-back.googlecode.com/svn/trunk@21 0639aa0d-7a2e-ecb4-53b4-565c565d6c1b

--- gyb.py
+++ gyb.py
@@ -23,7 +23,7 @@ For more information, see http://code.google.com/p/got-your-back/
 __program_name__ = 'Got Your Back: Gmail Backup'
 __author__ = 'Jay Lee'
 __email__ = 'jay@jhltechservices.com'
-__version__ = '0.06 Alpha'
+__version__ = '0.07 Alpha'
 __license__ = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 __db_schema_version__ = '2'
 
